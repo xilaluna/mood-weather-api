784e4760f074a25e7c009bc78434332db17c6fd4
Xila Luna <xilaluna2@gmail.com>
2021-06-27 18:51:51 -0700

    added routes and api implmentation

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-require("dotenv").config()
-import express from "express"
-import handlebars from "express-handlebars"
+import dotenv from "dotenv"
+import express, { response } from 'express'
+import exphbs from 'express-handlebars'
+import axios from 'axios'
+
+dotenv.config()
 
 const app = express()
 
-app.set('view engine')
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/weather', (req, res) => {
+  if(req.query.location) {
+    axios.get(`api.openweathermap.org/data/2.5/weather?q=${req.query.location}&units=imperial&appid=${process.env.API_KEY}`)
+    .then((response) => {
+      let weatherAspects = response
+      console.log('hello')
+      res.render("results", {weatherAspects})
+    })
+    .catch(console.error)
+  }
+}) 
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
