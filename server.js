e7602579c91c4c0e18d5d2ee2dc5739d0e570c92
Xila Luna <xilaluna2@gmail.com>
2021-06-29 14:14:02 -0700

    fixed https problema and succesfully added response onto page

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
 import dotenv from "dotenv"
-import express, { response } from 'express'
-import exphbs from 'express-handlebars'
-import axios from 'axios'
+import express from "express"
+import exphbs from "express-handlebars"
+import axios from "axios"
 
 dotenv.config()
 
 const app = express()
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs())
+app.set("view engine", "handlebars")
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get("/", (req, res) => {
+  res.render("home")
 })
 
-app.get('/weather', (req, res) => {
-  if(req.query.location) {
-    axios.get(`api.openweathermap.org/data/2.5/weather?q=${req.query.location}&units=imperial&appid=${process.env.API_KEY}`)
-    .then((response) => {
-      let weatherAspects = response
-      console.log('hello')
-      res.render("results", {weatherAspects})
-    })
-    .catch(console.error)
+app.get("/weather", (req, res) => {
+  if (req.query.location) {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&units=imperial&appid=${process.env.API_KEY}`
+      )
+      .then((response) => {
+        let weatherAspects = response.data
+        let name = weatherAspects.name
+        let weather = weatherAspects.weather[0].main
+        let temp = weatherAspects.main.temp
+        res.render("results", { name, weather, temp })
+      })
+      .catch(console.error)
   }
-}) 
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log("Example app listening on port 3000!")
 })
